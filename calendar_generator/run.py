e491ae3295d340c29057c86c49c17ba3e55ea538
Megan <60526470+meganlavengood@users.noreply.github.com>
2023-01-11 21:17:06 -0500

    update run.py to create comps page

--- calendar_generator/run.py
+++ calendar_generator/run.py
@@ -2,20 +2,53 @@ import pathlib
 import pandas as pd
 
 TEMPLATE_FILE_NAME = "template.txt"
+COMPOSITION_TEMPLATE_FILE_NAME = "composition_template.txt"
 DATA_FILE_NAME = "data.csv"
-OUTPUT_DIRECTORY = "../docs/_posts"
+POST_OUTPUT_DIRECTORY = "../docs/_posts"
+COMPOSITION_OUTPUT_DIRECTORY = '../docs'
 
 template = pathlib.Path(TEMPLATE_FILE_NAME).read_text()
+composition_template = pathlib.Path(COMPOSITION_TEMPLATE_FILE_NAME).read_text()
 
 data = pd.read_csv(DATA_FILE_NAME)
 
 data = data.fillna("")
 data = data.replace("none", "")
 
-for row in data.iterrows():
-    row_dict = row[1].to_dict()
-    day = template.format(**row_dict)
-    day = day.replace("[]()", "")
-    pathlib.Path(
-        f'{OUTPUT_DIRECTORY}/{row_dict["date"]}-{row_dict["r-title"].replace(" ", "-").lower()}.md'
-    ).write_text(day)
+def build_posts(data):
+    for row in data.iterrows():
+        row_dict = row[1].to_dict()
+        day = template.format(**row_dict)
+        day = day.replace("[]()", "")
+        pathlib.Path(
+            f'{POST_OUTPUT_DIRECTORY}/{row_dict["date"]}-{row_dict["r-title"].replace(" ", "-").lower()}.md'
+        ).write_text(day)
+
+def build_compositions(data, composition_template):
+    comp_keywords = parse_composition_keywords(composition_template)
+    comp_dict = {}
+    for row in data.iterrows():
+        row_dict = row[1].to_dict()
+        title = row_dict['a-title'].lower()
+        if 'project' in title:
+            for kw in comp_keywords:
+                if kw in title:
+                    comp_keywords.remove(kw)
+                    comp_dict[f'{kw}_url'] = row_dict['a-url']
+                    comp_dict[f'{kw}_date'] = row_dict['post-name']
+    compositions_footer = composition_template.split('\n')[-1]
+    compositions = composition_template[:composition_template.rfind('\n')].format(**comp_dict) + compositions_footer
+    pathlib.Path(f'{COMPOSITION_OUTPUT_DIRECTORY}/Compositions.md').write_text(compositions)
+
+
+def parse_composition_keywords(template):
+    result = []
+    for line in template.split('\n'):
+        if line.startswith('##'):
+            kw = line.split(' ')[1].lower()
+            if kw != 'submitting':
+                result.append(kw)
+    return result
+
+build_posts(data)
+build_compositions(data, composition_template)
--- docs/Compositions.md
+++ docs/Compositions.md
@@ -2,31 +2,31 @@
 layout: page
 title: Compositions
 permalink: /compositions/
+toc: false
 order: 3
 ---
 
 ## Blues
 
-* Instructions
-* Due XXX
+* [Instructions](https://viva.pressbooks.pub/openmusictheory/chapter/blues-melodies-and-the-blues-scale/#assignments)
+* Due: Tue, Feb 7
 
 ## Bebop
 
-* Instructions
-* Due XXX
+* [Instructions](https://viva.pressbooks.pub/openmusictheory/chapter/jazz-embellishing-chords/#assignments)
+* Due: Thu, Mar 2
 
 ## Wedding arrangement
 
-* Instructions
-* Due XXX
+* [Instructions](https://viva.pressbooks.pub/openmusictheory/chapter/rhythm-and-meter-in-pop-music/#assignments)
+* Due: Thu, Mar 30
 
 ## Songwriting
 
-* Instructions
-* Due XXX
+* [Instructions](https://gmuedu-my.sharepoint.com/:b:/g/personal/mlavengo_gmu_edu/EQXt8bU-pYpMvEC77ZwlMkEBamETDLnvOG28Pq7VYXH_iA?e=eTgPC7)
+* Due: Tue, Apr 25
 
 ## Submitting and revising
 
 All compositions should be submitted on Blackboard as **PDFs**. You may also submit sound files.
-
 All compositions may be revised. See [Syllabus]({{ '/syllabus/#graded-projects' | relative_url}}) for more information.
